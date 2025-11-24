a8f0f8bcf6a499ede81f12bbda6205eb335e7d64
Abdrahmanyahya <abdrahmanyahya21@gmail.com>
2025-11-24 11:49:55 +0200

    create users

--- src/Pages/Users/Users.jsx
+++ src/Pages/Users/Users.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 function Users() {
+const[users,setusers]=useState([]);
+const [isloding,setloding] = useState(true);
+const [eroor,seterror]=useState('');
+const getusers = async()=>{
+
+try{
+
+const responce = await axios.get(`${import.meta.env.VITE_BURL}/users`);
+console.log(responce.data.users);
+setusers(responce.data.users);
+
+
+}catch(err){
+console.log(err.message);
+seterror(err.message);
+}finally{
+setloding(false);
+}
+
+
+}
+
+useEffect(()=>{
+getusers();
+},[])
+
+
+if(isloding){
+    return <h2>Wait......</h2>
+}
+if(eroor){
+    return <h2>{eroor}</h2>
+    
+}
+
+
   return (
-    <div>
-      <h2>Users</h2>
+    <div className='p-5'>
+
+      <table className='table'>
+        <thead  >
+       <tr>
+        <th>id</th>
+        <th>Name</th>
+        <th>email</th>
+        <th>action</th>
+       </tr>
+
+
+        </thead>
+
+        <tbody>
+
+       {
+        users.map((user)=>{
+       return  <tr className='p-2' key={user.id}>
+             <td >{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td> </td>
+        </tr>
+
+        })
+       }
+        </tbody>
+      </table>
+
+
+
+
+
+
+       { console.log("render")};
+
+
     </div>
   )
 }
